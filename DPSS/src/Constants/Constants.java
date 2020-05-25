8b1643e74411d7f8b179f7fd6c869934e38ffbd3
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2020-05-25 04:09:01 -0400

    FEAT: Adding constant files containing Server informations

---
+++ DPSS/src/Constants/Constants.java
@@ -0,0 +1,14 @@
+package Constants;
+
+public class Constants {
+
+    /* Game server ports */
+    public static final int SERVER_IP_PORT_AMERICA = 132;
+    public static final int SERVER_IP_PORT_EUROPE = 93;
+    public static final int SERVER_IP_PORT_ASIA = 182;
+
+    public static final String SERVER_NAME_AMERICA = "AmericaServer";
+    public static final String SERVER_NAME_EUROPE = "EuropeServer";
+    public static final String SERVER_NAME_ASIA = "AsiaServer";
+
+}
